162b917be435152746a364dd0324b64aee61c2f5
Emeka <Jagobaadimora@gmail.com>
2024-04-25 09:59:29 +0100

    Added css styling with typewriter function in js

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
+function typeWriter(text, index, element) {
+    if (index < text.length) {
+        // Check if the current character is a line break
+        if (text.charAt(index) === '<' && text.substring(index, index + 4) === '<br>') {
+            // Insert a line break element
+            element.appendChild(document.createElement('br'));
+            // Skip the next 3 characters ('<br>')
+            setTimeout(() => typeWriter(text, index + 4, element), 25);
+        } else {
+            // Append the current character
+            element.innerHTML += text.charAt(index);
+            setTimeout(() => typeWriter(text, index + 1, element), 25);
+        }
+    }
+}
+
 function handleCommand(command) {
     const outputElement = document.getElementById('terminalOutput');
     outputElement.innerHTML = ''; // Clear previous output
 
-    switch (command) {
+    // Convert command to lowercase to ensure case insensitivity
+    const lowerCaseCommand = command.toLowerCase();
+
+    switch (lowerCaseCommand) {
         case 'help':
-            const helpText = 'Available commands: help - Show available commands. social - Display social networks. projects - Show projects. experience - Show experience. education - Show education. hobbies - Show hobbies.';
-            // Split the help text into individual characters
-            const characters = helpText.split('');
-            // Map each character to a span with a dynamically set animation delay
-            const animatedText = characters.map((char, index) => `<span style="animation-delay: ${index * 0.1}s;">${char}</span>`).join('');
-            outputElement.innerHTML = animatedText;
+            typeWriter('Available commands:<br>help - Show available commands.<br>social - Display social networks.<br>projects - Show projects.<br>experience - Show experience.<br>education - Show education.<br>hobbies - Show hobbies.<br>', 0, outputElement);
+            break;
+        case 'social':
+            typeWriter('Social:<br>LinkedIn: Emeka Adimora<br>Twitter: @EmekaAdimora<br>Instagram: @EmekaAdimora<br>', 0, outputElement);
+            break;
+        case 'projects':
+            typeWriter('Projects:<br>Project 1: Personal Portfolio<br>Project 2: Online Store<br>Project 3: Blogging Platform<br>', 0, outputElement);
+            break;
+        case 'experience':
+            typeWriter('Experience:<br>2018 - 2020: Junior Developer at XYZ Corp.<br>2021 - Present: Senior Developer at ABC Inc.<br>', 0, outputElement);
+            break;
+        case 'education':
+            typeWriter('Education:<br>Bachelor of Science in Computer Science, University of XYZ, 2018<br>Master of Science in Software Engineering, University of ABC, 2020<br>', 0, outputElement);
+            break;
+        case 'hobbies':
+            typeWriter('Hobbies:<br>Coding<br>Reading<br>Gaming<br>', 0, outputElement);
             break;
         // Add more cases for other commands here
         default:
-            outputElement.innerHTML = 'Command not found. For a list of commands, type \'help\'.<br/>';
+            typeWriter('Error: Command not found. For a list of commands, type \'help\'.<br>', 0, outputElement);
     }
 }
 
